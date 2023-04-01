8e649d122083889247f8e78ed4aecd5dfc679140
ESHAN GUPTA <es66han66@gmail.com>
2023-04-01 19:30:49 +0530

    CRUD APs with mongo no singleton

---
+++ CRUD_with_mongodb_no_singleton/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
+func CreatePersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var person Person
+	_ = json.NewDecoder(request.Body).Decode(&person)
+	collection := client.Database("goLangCRUDWithMongoPerson").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, person)
+	json.NewEncoder(response).Encode(result)
+}
+
+func GetPeopleEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var people []Person
+	collection := client.Database("goLangCRUDWithMongoPerson").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var person Person
+		cursor.Decode(&person)
+		people = append(people, person)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(people)
+}
+
+func GetPersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var person Person
+	collection := client.Database("goLangCRUDWithMongoPerson").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	// err := collection.FindOne(ctx, Person{ID: id}).Decode(&person) // it also works
+	// err := collection.FindOne(ctx, Person{Firstname: "Eshan"}).Decode(&person) // it also works
+	// err := collection.FindOne(ctx, bson.M{"firstname": "Eshan"}).Decode(&person) // it also works
+	err := collection.FindOne(ctx, bson.M{"_id": id}).Decode(&person) // it also works
+
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(person)
+}
+
+func DeletePersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	collection := client.Database("goLangCRUDWithMongoPerson").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	result, err := collection.DeleteOne(ctx, bson.M{"_id": id})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(result)
+}
+
+func UpdatePersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var person Person
+	json.NewDecoder(request.Body).Decode(&person)
+	collection := client.Database("goLangCRUDWithMongoPerson").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	result, err := collection.UpdateOne(ctx, bson.M{"_id": id}, bson.M{
+		"$set": person,
+	})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(result)
+}
+
+func main() {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
+	router := mux.NewRouter()
+	router.HandleFunc("/person", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/person/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/person/{id}", DeletePersonEndpoint).Methods("DELETE")
+	router.HandleFunc("/person/{id}", UpdatePersonEndpoint).Methods("PUT")
+	fmt.Println("Starting the application at port 8000...")
+	http.ListenAndServe(":8000", router)
+}

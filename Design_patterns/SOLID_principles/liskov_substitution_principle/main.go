5dae516167a3496915364ba803bf6b7741407a61
ESHAN GUPTA <es66han66@gmail.com>
2023-04-21 20:19:56 +0530

    Liskov substitution principle

---
+++ Design_patterns/SOLID_principles/liskov_substitution_principle/main.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+type Sized interface {
+	GetWidth() int
+	SetWidth(width int)
+	GetHeight() int
+	SetHeight(height int)
+}
+
+type Rectangle struct {
+	width, height int
+}
+
+//     vvv !! POINTER
+func (r *Rectangle) GetWidth() int {
+	return r.width
+}
+
+func (r *Rectangle) SetWidth(width int) {
+	r.width = width
+}
+
+func (r *Rectangle) GetHeight() int {
+	return r.height
+}
+
+func (r *Rectangle) SetHeight(height int) {
+	r.height = height
+}
+
+// modified LSP
+// If a function takes an interface and
+// works with a type T that implements this
+// interface, any structure that aggregates T
+// should also be usable in that function.
+type Square struct {
+	Rectangle
+}
+
+func NewSquare(size int) *Square {
+	sq := Square{}
+	sq.width = size
+	sq.height = size
+	return &sq
+}
+
+/*
+though in below 2 functions attached tp Square type, SetWidth and SetHeight are logically correct but they defy the LSP because
+this function was designed with a goal to only set width or height and not touch the other one, here the parent type i.e. Rectangle
+works correctly but it's derived type Square doesn't work same
+*/
+func (s *Square) SetWidth(width int) {
+	s.width = width
+	s.height = width
+}
+
+func (s *Square) SetHeight(height int) {
+	s.width = height
+	s.height = height
+}
+
+type Square2 struct {
+	size int
+}
+
+func (s *Square2) Rectangle() Rectangle {
+	return Rectangle{s.size, s.size}
+}
+
+func UseIt(sized Sized) {
+	width := sized.GetWidth()
+	sized.SetHeight(10)
+	expectedArea := 10 * width
+	actualArea := sized.GetWidth() * sized.GetHeight()
+	fmt.Print("Expected an area of ", expectedArea,
+		", but got ", actualArea, "\n")
+}
+
+func main() {
+	rc := &Rectangle{2, 3}
+	UseIt(rc)
+
+	sq := NewSquare(5)
+	UseIt(sq)
+}
